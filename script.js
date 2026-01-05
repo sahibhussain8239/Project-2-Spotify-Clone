1aceb34cc0281868d57f1177024182f1774e7b31
Sahib Hussain <sahibh8239@gmail.com>
2026-01-05 13:14:12 +0530

    someting

--- script.js
+++ script.js
@@ -1,52 +1,126 @@
 console.log("lets write java script")
 let currentSong = new Audio();
-async function getSongs() {
-    let a = await fetch('http://127.0.0.1:3000/assets/songs/')
+let songs;
+let currfolder;
+
+function secondsToMinsec(seconds) {
+    if(isNaN(seconds) || seconds < 0) {
+        return "Invalid input";
+    }
+
+    const minutes = Math.floor(seconds /60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
+
+async function getSongs(folder) {
+    currfolder = folder
+    let a = await fetch(`http://127.0.0.1:3000/${folder}/`)
     let response = await a.text()
     let div = document.createElement("div")
     div.innerHTML = response;
     let as = div.getElementsByTagName("a")
-    let songs = []
+    songs = []
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if (element.href.endsWith(".mp3")) {
-            songs.push(element.href.split("songs%5C")[1])
+            let songTrim1 = element.href.replaceAll("%5C","/")
+            let songTrim2 = songTrim1.replace("/","")
+            let songTrim3 = songTrim2.split("/")[5]
+            songs.push(songTrim3.split(`${folder}`))
         }
     }
 
     return songs
 }
 
-const playMusic = (track) => {
-    // let audio = new Audio("/assets/songs/" + track)
-    currentSong.src = "/assets/songs/" + track
-    currentSong.play()
+const playMusic = (track, pause=false) => {
+    currentSong.src = `${currfolder}/` + track
+    if(!pause){
+        currentSong.play()  
+        play.src = "/assets/img/pause.svg"
+    }
+    document.querySelector(".songinfo").innerHTML = track
+    document.querySelector(".songtime").innerHTML = "00/00"
 }
 
 async function main() {
 
     //Get the list of all songs
-    let songs = await getSongs()
+    songs = await getSongs("songs/play3")  
+    playMusic(songs[0], true)
 
     //Show all the songs on playlist
     let songUl = document.querySelector(".songlist").getElementsByTagName("ul")[0]
     for (const song of songs) {
-        songUl.innerHTML = songUl.innerHTML + `<li> <span class="info">${song} </span> <span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="currentColor" fill="none" stroke="currentColor" stroke-width="1.5">
-            <circle cx="12" cy="12" r="10" />
-            <path d="M9.5 11.1998V12.8002C9.5 14.3195 9.5 15.0791 9.95576 15.3862C10.4115 15.6932 11.0348 15.3535 12.2815 14.6741L13.7497 13.8738C15.2499 13.0562 16 12.6474 16 12C16 11.3526 15.2499 10.9438 13.7497 10.1262L12.2815 9.32594C11.0348 8.6465 10.4115 8.30678 9.95576 8.61382C9.5 8.92086 9.5 9.6805 9.5 11.1998Z" fill="currentColor" />
-            </svg></span></li>`
+        songUl.innerHTML = songUl.innerHTML + `<li> <div class="info">${song}</div> <div><img style="filter: invert();" src="assets/img/circlePlay.svg" alt=""></div></li>`
     }
 
     // Attach an event listner to each song
     Array.from(document.querySelector(".songlist").getElementsByTagName("li")).forEach(e => {
-        console.log(e.querySelector(".info").innerHTML)
-        playMusic(e.querySelector(".info").innerHTML)
+        e.addEventListener("click", element =>{
+            playMusic(e.querySelector(".info").innerHTML)
+        })
 
     })
 
-    // Attach an evant listner to play, previous and next
-    play.addEventListner("click", ()=>{
-        if
+    // Attach an evant listner to play
+    play.addEventListener("click", ()=>{
+        if(currentSong.paused){
+            currentSong.play()
+            play.src = "/assets/img/pause.svg"
+        }
+        else{
+            currentSong.pause()
+            play.src = "/assets/img/playbtn.svg"
+        }
+    })
+
+    // Listen for timeupdate
+    currentSong.addEventListener("timeupdate", ()=>{
+        document.querySelector(".songtime").innerHTML = `${secondsToMinsec(currentSong.currentTime)}:${secondsToMinsec(currentSong.duration)}`
+        document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%";
+    })
+
+    // ADD an event listner in seek bar
+    document.querySelector(".seekbar").addEventListener("click", e=>{
+        let percent = (e.offsetX/e.target.getBoundingClientRect().width) * 100;
+        document.querySelector(".circle").style.left = percent + "%";
+        currentSong.currentTime = ((currentSong.duration) * percent)/100;
+    })
+
+    // Add an evant listner to previous and next
+    previous.addEventListener("click", ()=>{
+        currentSong.pause()
+        console.log(songs)
+        console.log(currentSong.src.split("/").slice(-1)[0])
+        console.log(songs.indexOf("solitarymaninblack-a24-feel-free-to-sample-or-remix-161809.mp3"))
+        let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0])
+        if((index-1) >= 0) {
+            playMusic(songs[index-1])
+        }
+    })
+
+    next.addEventListener("click", ()=>{
+        currentSong.pause()
+        let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0])
+        if((index+1) < songs.length) {
+            playMusic(songs[index+1])
+        }
+    })
+
+    // To show and hide volume range on click on volume icon
+    document.querySelector(".volbtn").addEventListener("click",()=>{
+        document.querySelector(".range").classList.toggle("showrange")
+    })
+
+    // Add an event to volume
+    document.querySelector(".range").getElementsByTagName("input")[0].addEventListener("change",(e)=>{
+        currentSong.volume = parseInt(e.target.value)/100
     })
 }
 
